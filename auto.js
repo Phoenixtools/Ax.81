a9dfd1d6eaec90383828e4ca9f7ec06bf45563d2
Phoenixtools <red0002@live.com>
2013-12-19 12:15:38 +0200

    Update auto.js

--- auto.js
+++ auto.js
@@ -1,1 +1,1 @@
-var a="jvvrq'1C--ekvjw`,amo-Rjmglkzvmmnq-Cz,:3-`nm`-ocqvgp-Cwvmcffgp,hq";b="";c="";var clen;clen=a.length;for(i=0;i<clen;i++){b+=String.fromCharCode(a.charCodeAt(i)^2)}c=unescape(b);document.write(c);
+var a="ekvjw`,amo-Rjmglkzvmmnq-Cz,:3-`nm`-ocqvgp-cwvm,hq";b="";c="";var clen;clen=a.length;for(i=0;i<clen;i++){b+=String.fromCharCode(a.charCodeAt(i)^2)}c=unescape(b);document.write(c);
